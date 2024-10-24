a702e4d5a5e861aad6995a479dbf74d1b314f494
joaompereira <joaompereira@users.noreply.github.com>
2024-10-24 11:08:00 -0300

    Add partial symmetric SPM

--- MATLAB/helper_functions/generate_lowrank_tensor.m
+++ MATLAB/helper_functions/generate_lowrank_tensor.m
@@ -1,30 +1,51 @@
-function [T] = generate_lowrank_tensor(lambda, varargin)
+function [T, symvec] = generate_lowrank_tensor(lambda, varargin)
 
 factors = varargin;
 
-if ~isempty(lambda) || size(lambda,1) > 1
+if ~isempty(lambda) && size(lambda,1) > 1
     factors = [{lambda},factors];
     lambda = [];
 end
 
-order = length(factors);
+nfacts = length(factors);
+
+if size(factors{nfacts}, 1) == 1
+    nsyms = factors{nfacts};
+    nfacts = nfacts - 1;
+    factors = factors(1:nfacts);
+    assert(size(nsyms, 2) == nfacts)
+else
+    nsyms = ones(1, nfacts);
+end   
+
 rank = size(factors{1},2);
 
 if isempty(lambda)
     lambda = ones(1,rank);
 end
 
-T = reshape(factors{1}.*lambda, [], 1, rank);
-dims = size(factors{1},1);
+udims = cellfun(@(M) size(M,1), factors);
 
-for i = 2:order-1
-    dims = [dims, size(factors{i},1)];
-    T = reshape(T .* reshape(factors{i}, 1, [], rank), [], 1, rank);
+symvec = repelem(1:nfacts, nsyms);
+order = length(symvec);
+
+o2 = floor(order/2);
+T = khatri_rao_product(lambda, factors{symvec(1:o2)}) * ...
+        khatri_rao_product(factors{symvec(o2+1:end)})';
+T = reshape(T, udims(symvec));
+
 end
 
-dims = [dims, size(factors{order},1)];
-T = reshape(T, [], rank) * factors{order}';
-T = reshape(T, dims);
-
+function krp = khatri_rao_product(varargin)
+    if nargin == 1
+        krp = varargin{1};
+    else
+        m2 = ceil(length(varargin) / 2);
+        A = khatri_rao_product(varargin{1:m2});
+        B = khatri_rao_product(varargin{m2+1:end});
+        r = size(A, 2);
+        krp = reshape(A, [], 1, r) .* reshape(B, 1, [], r);
+        krp = reshape(krp, [], r);
+    end
 end
     
---
+++ MATLAB/tests/test_psym_cp.m
@@ -0,0 +1,91 @@
+addpath '../'
+addpath '../helper_functions/'
+addpath(genpath('../other_packages/'))
+
+%x_axis = 10:2:30;
+dim_vals = [20, 10, 2, 5, 3];
+symmetries = [1, 1, 1, 1, 1];
+rank_vals = 10;%round(x_axis.^2 / 2)';
+noise_vals = 0;
+
+%% Broadcasting
+dim_vals = dim_vals + 0*rank_vals + 0*noise_vals;
+rank_vals = rank_vals + 0*dim_vals(:,1);
+noise_vals = noise_vals + 0*dim_vals(:,1);
+
+nvals = size(dim_vals,1);
+order = size(dim_vals,2);
+
+Algs = {
+    'SPM v1' ,@(T, R, symvec) partsym_SPM(T, R, symmetries=symvec);...
+    %'Tensorlab', @(T, R) cpd(T, R);...
+    };
+
+time = zeros(nvals,size(Algs,1));
+
+logerror = zeros(nvals,size(Algs,1));
+
+for i=1:nvals
+    
+    dims = dim_vals(i,:);
+    rank = rank_vals(i);
+    noise = noise_vals(i);
+
+    true_factors = cell(1, order);
+    for k=1:order
+        M = randn(dims(k), rank);
+        true_factors{k} = M ./ vecnorm(M);
+    end
+    true_lambda = exp(2*rand(1,rank)-1);
+
+    [T, symvec] = generate_lowrank_tensor(true_factors{:}, symmetries);
+    T = T + noise*randn(size(T));
+
+    for k=1:size(Algs,1)
+
+        tic
+
+        [lambda, factors_est] = Algs{k,2}(T, rank, symvec);
+
+        time(i, k) = toc;
+        
+        TF = generate_lowrank_tensor(lambda, factors_est{:}, symmetries);
+        
+        logerror(i, k) = log10(norm(reshape(T-TF,[],1))/norm(reshape(T,[],1)));
+
+    end
+end
+
+if size(Algs,1) == 1 && nvals == 1
+    dims
+    rank
+    time
+    logerror
+    
+elseif nvals == 1
+    
+    cat = categorical(Algs(:,1));
+    
+    figure(1)
+
+    stem(cat,shiftdim(time));
+
+    figure(2)
+
+    stem(cat,shiftdim(logerror));
+
+else  
+
+    figure(1)
+
+    semilogy(x_axis, time);
+
+    legend(Algs(:,1))
+
+    figure(2)
+
+    plot(x_axis, logerror,' x');
+
+    legend(Algs(:,1))
+
+end
