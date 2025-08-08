c60496e1a74d3d22ab83a438e15d07abdf1b01a9
joaompereira <joaompereira@users.noreply.github.com>
2025-08-08 11:35:15 -0400

    Add lsq_refine option to both SPM implementations

--- python/part_sym_SPM.py
+++ python/part_sym_SPM.py
@@ -1,6 +1,7 @@
 import numpy as np
-from utils import norm, khatri_rao_power, option_parser, \
-    compiler_decorator, prange, pos, isbool, dormqr, lapack
+from utils import norm, khatri_rao_power, khatri_rao_product, \
+    option_parser, compiler_decorator, prange, pos, isbool, \
+    dormqr, lapack
 from scipy.linalg import svd
 from time import time
 from math import sqrt
@@ -66,9 +67,8 @@ def pm_refinement_iteration(Vt, A_, rho, maxiter, gradtol, Ak, Bk):
             ind = np.argmax(ucorr)
             if ucorr[ind] > rho:
                 Ak -= corr[ind] * A_[:, ind]
-                Ak /= norm(Ak)
-                s = np.sign(corr[ind])
-                Ak = gamma * Ak + (rho * s) * A_[:, ind] 
+                Ak *= gamma / norm(Ak)
+                Ak += (rho * np.sign(corr[ind])) * A_[:, ind] 
 
         VCk = np.dot(Ck, V_C).reshape(m, n)
         Bk_new = np.dot(Ak, VCk)
@@ -122,7 +122,8 @@ def spm_21sym_robust(T, r=None, **kwargs):
                          ('rho', .7, pos),
                          ('w_out', True, isbool),
                          ('return_stats', False, isbool),
-                         ('O_version', False, isbool))
+                         ('O_version', False, isbool),
+                         ('lsq_refine', None, None))
 
     m_, m, n = T.shape
     assert m_ == m, "Tensor T must be symmetric."
@@ -144,8 +145,9 @@ def spm_21sym_robust(T, r=None, **kwargs):
     U = U[:, :r]
     O = np.eye(r)
 
-    A = np.zeros((m, r))
-    B = np.zeros((n, r))
+    A = np.zeros((m, r), order='F')
+    B = np.zeros((n, r), order='F')
+    l = np.zeros((r,))
     
     stats = []
 
@@ -182,10 +184,17 @@ def spm_21sym_robust(T, r=None, **kwargs):
         statsk.sort(reverse=True, key=lambda stat: stat['f'])
         #stats.append(statsk)
         stats.append({'niter': iter, 'err': err, 'f': f, 'deflate_stats': statsk})
+        
+        alphaU = np.dot(Ak, U)
+        alphaV = np.dot(Vt, (Ak.reshape(-1, 1) * Bk.reshape(1, -1)).reshape(-1))
+
+        # Solve for lambda
+        D1alphaU = np.dot(alphaU, D1)
+        D1alphaV = np.dot(D1, alphaV)
+        l[k] = norm(alphaU) * norm(alphaV) / np.dot(alphaV, D1alphaU)
 
         if k < r-1:
-            alphaU = np.dot(Ak, U)
-            
+                        
             # Update V and D using Householder reflection
             # Calculate the new matrix D and the new subspace
             # Use Householder reflection to update V and D
@@ -215,9 +224,18 @@ def spm_21sym_robust(T, r=None, **kwargs):
                 D1_deflated = D1_deflated[:, 1:]
                 
         A[:, k] = Ak
-        
-    G = np.square(np.dot(A.T, A))
-    B = np.linalg.solve(G, np.dot(khatri_rao_power(A, 2).T, np.reshape(T, (-1, n)))).T
+        B[:, k] = Bk
+    
+    if opts.lsq_refine == "B":    
+        G = np.square(np.dot(A.T, A))
+        B = np.linalg.solve(G, np.dot(khatri_rao_power(A, 2).T, np.reshape(T, (-1, n)))).T
+    elif opts.lsq_refine == "lambda":
+        G = np.square(np.dot(A.T, A)) * np.dot(B.T, B)
+        Z = np.dot(np.reshape(T, (m, -1)), khatri_rao_product(A, B))
+        u = np.sum(A * Z, axis=0)
+        B *= np.expand_dims(np.linalg.solve(G, u), axis=0)
+    else:
+        B *= np.expand_dims(l, axis=0)
     
     if opts.return_stats:
         return A, B, stats
@@ -251,7 +269,8 @@ def spm_21sym(T, r=None, **kwargs):
                          ('eigtol', 1e-8, pos),
                          ('ftol', 1e-2, pos),
                          ('w_out', True, isbool),
-                         ('return_stats', False, isbool))
+                         ('return_stats', False, isbool),
+                         ('lsq_refine', None, None))
 
     m_, m, n = T.shape
     assert m_ == m, "Tensor T must be symmetric."
@@ -272,6 +291,7 @@ def spm_21sym(T, r=None, **kwargs):
 
     A = np.zeros((m, r))
     B = np.zeros((n, r))
+    l = np.zeros((r,))
     
     stats = []
 
@@ -310,7 +330,7 @@ def spm_21sym(T, r=None, **kwargs):
         # Solve for lambda
         D1alphaU = np.dot(alphaU, D1)
         D1alphaV = np.dot(D1, alphaV)
-        lambdak = norm(alphaU) * norm(alphaV) / np.dot(alphaV, D1alphaU)
+        l[k] = norm(alphaU) * norm(alphaV) / np.dot(alphaV, D1alphaU)
 
         if k < r-1:
             # Update V and D using Householder reflection
@@ -330,11 +350,19 @@ def spm_21sym(T, r=None, **kwargs):
             U = U[:, 1:]
 
         A[:, k] = Ak
-        B[:, k] = lambdak * Bk
-        
-    G = np.square(np.dot(A.T, A))
-    B = np.linalg.solve(G, np.dot(khatri_rao_power(A, 2).T, np.reshape(T, (-1, n)))).T
-
+        B[:, k] = Bk
+    
+    if opts.lsq_refine == "B":    
+        G = np.square(np.dot(A.T, A))
+        B = np.linalg.solve(G, np.dot(khatri_rao_power(A, 2).T, np.reshape(T, (-1, n)))).T
+    elif opts.lsq_refine == "lambda":
+        G = np.square(np.dot(A.T, A)) * np.dot(B.T, B)
+        Z = np.dot(np.reshape(T, (m, -1)), khatri_rao_product(A, B))
+        u = np.sum(A * Z, axis=0)
+        B *= np.expand_dims(np.linalg.solve(G, u), axis=0)
+    else:
+        B *= np.expand_dims(l, axis=0)
+    
     if opts.return_stats:
         return A, B, stats
     else:
@@ -343,15 +371,16 @@ def spm_21sym(T, r=None, **kwargs):
 if __name__ == '__main__':
     # Example usage
 
-    m = 200
-    n = 200
-    r = 100
+    m = 100
+    n = 75
+    r = 50
 
     A = np.random.randn(m, r)
     B = np.random.randn(n, r)
 
     T = np.dot(khatri_rao_power(A, 2), B.T).reshape(m, m, n)
     
+    print("## Example 1 - Noiseless ##")
     print(":: SPM ::")
     start = time()
     A_, B_, stats = spm_21sym(T, r=r, maxiter=1000, ntries=3,
@@ -369,16 +398,9 @@ if __name__ == '__main__':
     T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
     print("Error:", np.linalg.norm(T.reshape(-1) -
           T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
-    
-    print(":: Robust SPM (O-version) ::")
-    start = time()
-    A_, B_, stats = spm_21sym_robust(T, r=r, maxiter=1000, ntries=3,
-                       gradtol=1e-10, ftol=1e-5, return_stats=True, O_version=True)
-    print("Time taken:", time() - start)
-    T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
-    print("Error:", np.linalg.norm(T.reshape(-1) -
-          T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
 
+    print("\n## Example 2 - Noisy ##")
+    
     A = np.random.randn(m, r)
     B = np.random.randn(n, r)
 
@@ -394,7 +416,25 @@ if __name__ == '__main__':
     print("Error:", np.linalg.norm(T.reshape(-1) -
           T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
     
-    print(":: Robust SPM ::")
+    print(":: SPM (lambda lsq refine) ::")
+    start = time()
+    A_, B_, stats = spm_21sym(T_noisy, r=r, maxiter=5000, ntries=3,
+                       gradtol=1e-10, ftol=1e-1, return_stats=True, lsq_refine='lambda')
+    print("Time taken:", time() - start)
+    T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
+    print("Error:", np.linalg.norm(T.reshape(-1) -
+          T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
+    
+    print(":: SPM (B lsq refine) ::")
+    start = time()
+    A_, B_, stats = spm_21sym(T_noisy, r=r, maxiter=5000, ntries=3,
+                       gradtol=1e-10, ftol=1e-1, return_stats=True, lsq_refine='B')
+    print("Time taken:", time() - start)
+    T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
+    print("Error:", np.linalg.norm(T.reshape(-1) -
+          T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
+    
+    print(":: Robust SPM (no refinement) ::")
     start = time()
     A_, B_, stats = spm_21sym_robust(T_noisy, r=r, maxiter=5000, ntries=3,
                        gradtol=1e-10, ftol=1e-1, return_stats=True)
@@ -403,10 +443,19 @@ if __name__ == '__main__':
     print("Error:", np.linalg.norm(T.reshape(-1) -
           T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
     
-    print(":: Robust SPM (O-version) ::")
+    print(":: Robust SPM (lambda lsq refine) ::")
     start = time()
     A_, B_, stats = spm_21sym_robust(T_noisy, r=r, maxiter=5000, ntries=3,
-                       gradtol=1e-10, ftol=1e-1, return_stats=True, O_version=True)
+                       gradtol=1e-10, ftol=1e-1, return_stats=True, lsq_refine='lambda')
+    print("Time taken:", time() - start)
+    T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
+    print("Error:", np.linalg.norm(T.reshape(-1) -
+          T_.reshape(-1)) / np.linalg.norm(T.reshape(-1)))
+    
+    print(":: Robust SPM (B refinement) ::")
+    start = time()
+    A_, B_, stats = spm_21sym_robust(T_noisy, r=r, maxiter=5000, ntries=3,
+                       gradtol=1e-10, ftol=1e-1, return_stats=True, lsq_refine='B')
     print("Time taken:", time() - start)
     T_ = np.dot(khatri_rao_power(A_, 2), B_.T).reshape(m, m, n)
     print("Error:", np.linalg.norm(T.reshape(-1) -
